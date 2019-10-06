cbb34434dac2b980118ff7728bc9c37231ad0735
Chad Dattilio <cdattilio@lyonscg.com>
2019-10-05 23:49:50 -0400

    Updates

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -30,9 +30,17 @@ def do_search() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> str:
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-        return escape(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 @app.route('/')
